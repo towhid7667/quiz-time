350990908de986b828139a0087ba41ebcbd5068e
towhid7667 <towhidulhq@gmail.com>
2022-10-12 23:53:47 +0600

    ended

--- src/components/HomePage/TopicCard.js
+++ src/components/HomePage/TopicCard.js
@@ -8,19 +8,19 @@ const TopicCard = (props) => {
     return (
         <>
             <div className="col-lg-4 col-md-6 ">
-                <div className="card border-dark mb-3" style={{ maxWidth: "540px" }}>
+                <div className="card border-light mb-3 bg-success" style={{ maxWidth: "540px" }}>
                     <div className="row g-0">
                         <div className="d-flex col-md-4 align-items-center justify-content-center p-2    ">
-                            <img src={logo} className="img-fluid rounded-start border border-dark border-3" alt={logo} />
+                            <img src={logo} className="img-fluid rounded-start border border-light border-3" alt={logo} />
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
 
-                                <h5 className="card-title fs-3 text-center">{name}</h5>
+                                <h5 className="card-title fs-3 text-center text-light">{name}</h5>
 
-                                <p className="card-text text-center"><small className="text-muted">Total: {total}</small></p>
+                                <p className="card-text text-center"><small className="text-light">Total: {total}</small></p>
 
-                                <Link className="btn btn-outline-dark float-end mb-2" to={`/quiz/${id}`}>Lets Go</Link>
+                                <Link className="btn btn-outline-light float-end mb-2" to={`/quiz/${id}`}>Lets Go</Link>
 
                             </div>
                         </div>
